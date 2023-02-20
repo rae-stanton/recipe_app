fa2d58c42c7f806c89a572360e46b53d72cb2366
Rae Stanton <raestanton93@gmail.com>
2023-02-20 12:41:44 -0800

    Working on seed user/user validations
    Added seed user, working on adding username/name and "signup" validations on back end.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
   has_many :recipes, foreign_key: 'author_id'
+
+  #validates :username, :password, presence: true
+  #validates :username, uniqueness: {message: "The username %{value} already exists!"}
+  #validates :password, length: {in: 6..15}, on: :create
+  
 end
