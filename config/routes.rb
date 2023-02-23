b2c93c1045a90f8baa273a98f263ac6f9812cfa1
Rae Stanton <raestanton93@gmail.com>
2023-02-22 17:29:26 -0800

    Update routes file
    This moves the signup page route, and connects it with the UsersController.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'users/new'
   root 'static_pages#home'
   get  "/about",   to: "static_pages#about"
-  get "/signup", to: "static_pages#signup"
+  get '/signup'  => 'users#new'
+  resources :users
  end
