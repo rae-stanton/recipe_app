f1fef1d82cf8e8cd0a394fc280ae4120c4f43b27
Rae Stanton <raestanton93@gmail.com>
2023-02-22 17:30:49 -0800

    Update UsersController
    This adds a couple of updated controller actions for the User resource.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,21 @@
 class UsersController < ApplicationController
-  def create
-    user = User.create()
+  def new
+    @user = User.new
+  end
+
+  def create 
+    @user = User.new(user_params)    # Not the final implementation!
+    
+    if @user.save
+      # Handle a successful save.
+    else
+      render 'new', status: :unprocessable_entity
+    end
   end
 
   private
+
   def user_params
-    params.permit(:username, :password)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
-  
 end
