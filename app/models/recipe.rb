3d6e98f4ba74e3713c622f02803d93def4f30ee1
Rae Stanton <raestanton93@gmail.com>
2023-02-25 13:50:00 -0800

    Difficulty enum with tests
    Added enum for difficulty and added tests validating changes.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,4 +2,13 @@ class Recipe < ApplicationRecord
   belongs_to :author, class_name: "User"
 
   has_many :ingredients
+  
+  enum difficulty: {
+    beginner: 0,
+    intermediate: 1,
+    advanced: 2,
+    expert: 3
+  }
+  validates :difficulty, presence: true
+  validates :name, presence: true
 end
--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -2,10 +2,31 @@ require 'rails_helper'
 
 RSpec.describe Recipe, type: :model do
   let(:faust) { User.create(name: "Faust") }
-  let(:recipe) { Recipe.create(name: "Eggplant Hee Hee", author: faust) }
+  let(:recipe) { Recipe.create(name: "Eggplant Hee Hee", author: faust, difficulty: "beginner") }
 
   it 'has an author' do
     expect(recipe.author.name).to eq("Faust")
+  end
 
+  describe "can change difficulty" do
+    it "has a beginner difficulty" do
+      expect(recipe.difficulty).to eq("beginner")
+    end
+
+  
+    it "has an intermediate difficulty" do
+      recipe.intermediate! 
+      expect(recipe.difficulty).to eq("intermediate")
+    end
+
+    it "has an advanced difficulty" do
+      recipe.advanced!
+      expect(recipe.difficulty).to eq("advanced")
+    end
+
+    it "has an expert difficulty" do
+      recipe.expert!
+      expect(recipe.difficulty).to eq("expert")
+    end
   end
 end
