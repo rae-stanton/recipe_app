b74ec2b862a302c416441423a1c9d349727822b9
Rae Stanton <raestanton93@gmail.com>
2023-02-25 11:11:04 -0800

    Added description to recipe table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_16_232414) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_25_190549) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,6 +24,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_16_232414) do
     t.datetime "updated_at", null: false
     t.bigint "author_id"
     t.text "name"
+    t.text "description"
     t.index ["author_id"], name: "index_recipes_on_author_id"
   end
 
