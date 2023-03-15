e6fadda6d1ab2e93915c6d78ba4475a32a27825e
Rae Stanton <raestanton93@gmail.com>
2023-03-15 12:02:45 -0700

    Added annotate gem
    This adds the annotate gem to our app. It updates a TON of files with annotations from our schema that show in the models. Probably going to be a massive diff here for that reason.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,21 @@
+# == Schema Information
+#
+# Table name: recipes
+#
+#  id          :bigint           not null, primary key
+#  description :text
+#  difficulty  :integer
+#  favorite    :boolean          default(FALSE), not null
+#  name        :text
+#  notes       :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  author_id   :bigint
+#
+# Indexes
+#
+#  index_recipes_on_author_id  (author_id)
+#
 class Recipe < ApplicationRecord
   belongs_to :author, class_name: "User"
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
+# == Route Map
+#
+
 Rails.application.routes.draw do
   root 'static_pages#home'
   get  "/about",   to: "static_pages#about"
