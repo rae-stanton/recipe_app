cb8a10cbe90790f089449251cabd5e79aa553040
Rae Stanton <raestanton93@gmail.com>
2023-03-16 16:11:18 -0700

    Tests for recipe show page
    These tests are for the favorite/not favorited buttons on the recipe show page.

--- spec/features/recipes_show_spec.rb
+++ spec/features/recipes_show_spec.rb
@@ -2,6 +2,7 @@ require "rails_helper"
 
 RSpec.describe "Recipes Show Page",
                 type: :feature do
+
     let!(:user) do
     User.create(
       name: "foosta",
@@ -19,6 +20,27 @@ RSpec.describe "Recipes Show Page",
       favorite: true
     )
   end
+
+  it "user clicks on a recipe from index page,
+      user sees button with text 'favorited recipe'" do
+
+    visit recipes_path
+    page.find_link(id: "button_#{recipe.id}").click
+    expect(page).to have_content(recipe.name)
+    click_button "Favorited Recipe"
+  end
+
+  it "user visits recipe index page,
+      user clicks on a recipe,
+      user can favorite that recipe" do
+    recipe.favorite = false
+    recipe.save
+    recipe.reload
+
+    visit recipes_path
+    page.find_link(id: "button_#{recipe.id}").click
+    click_button "Favorite This Recipe"
+  end
 end
 
-#Not yet implemented
+
