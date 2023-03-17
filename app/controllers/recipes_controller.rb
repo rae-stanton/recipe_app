922e42bdf3727a840a4612ccb72bea7b820c2e73
Paul Sandhu <spaulsandhu@gmail.com>
2023-03-17 16:05:32 -0700

    Fix spacing

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -7,23 +7,23 @@ class RecipesController < ApplicationController
     @recipe = Recipe.find(params[:id])
   end
 
-	def update
-		@recipe = Recipe.find(params[:id])
-		@recipe.update(favorite: "true")
-		@recipe.save
-	end
+  def update
+    @recipe = Recipe.find(params[:id])
+    @recipe.update(favorite: "true")
+    @recipe.save
+  end
 
-	def favorite # HTTP POST request
+  def favorite # HTTP POST request
     @recipe = Recipe.find(params[:recipe_id])
 
-	  if @recipe.favorite == false
-	    @recipe.update(favorite: true)
-	  else
-	    @recipe.update(favorite: false)
-		end
-	  #if recipe not favorite, update to true
-	  # else recipe is favorite update to false
-	  #recipe.save
-	  redirect_to recipe_path(id: @recipe.id)
-	end
+    if @recipe.favorite == false
+      @recipe.update(favorite: true)
+    else
+      @recipe.update(favorite: false)
+    end
+    #if recipe not favorite, update to true
+    # else recipe is favorite update to false
+    #recipe.save
+    redirect_to recipe_path(id: @recipe.id)
+  end
 end
