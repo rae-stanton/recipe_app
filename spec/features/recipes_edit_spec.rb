5e8b938c25ebed88c7c29cd497ee8c0f795ccfcd
Rae Stanton <raestanton93@gmail.com>
2023-03-24 17:05:27 -0700

    Feature spec for recipe edit action
    This finishes updating all specs for the updated recipe controller/model to reflect the description assertion (I know this is a bigger PR) I definitely need to do separate branches in these situations in the future. BUT this adds assertions for correct rendering of the edit page and for an invalid scenario with error message.

--- spec/features/recipes_edit_spec.rb
+++ spec/features/recipes_edit_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 RSpec.describe "Recipes Edit Page",
-                type: :feature do
+               type: :feature do
   let!(:user) do
     User.create(
       name: "foosta",
@@ -13,15 +13,35 @@ RSpec.describe "Recipes Edit Page",
   let!(:recipe) do
     Recipe.create(
       name: "Eggplant Hee Hee",
-      description: "something",
+      description: "something good",
       difficulty: "beginner",
       author: user
     )
   end
 
-  it "user clicks on recipe,
+  scenario "user clicks on recipe,
   user visits full recipe page,
   user edits recipe,
-  recipe saves or gets an error" do
+  recipe gets updated if valid" do
+    visit recipes_path
+    expect(page).to have_content("Recipes")
+    page.find_link(id: "button_#{recipe.id}").click
+    expect(page).to have_content(recipe.name)
+    click_link "Edit Recipe"
+    expect(page).to have_content("Edit Recipe")
+  end
+
+  scenario "user clicks on recipe,
+  user tries to edit recipe,
+  but the recipe is not valid" do
+    visit recipes_path
+    expect(page).to have_content("Recipes")
+    page.find_link(id: "button_#{recipe.id}").click
+    expect(page).to have_content(recipe.name)
+    click_link "Edit Recipe"
+    expect(page).to have_content("Edit Recipe")
+    fill_in "Description", with: "No"
+    click_button "Update Recipe"
+    expect(page).to have_content("Description is too short (minimum is 10 characters)")
   end
 end
