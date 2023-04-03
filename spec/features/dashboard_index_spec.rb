0bb9a95116f78fb6ff77c834842c3d70f1b5b0f0
Rae Stanton <raestanton93@gmail.com>
2023-04-03 14:44:34 -0700

    User dashboard add-ons
    This adds an area below the carousel that allows a user to create a new recipe or view all of the recipes from their dashboard. Not final styling by any means, but the function is most important for now. Test has been added for the dash.

---
+++ spec/features/dashboard_index_spec.rb
@@ -0,0 +1,28 @@
+require "rails_helper"
+
+RSpec.describe "User Dashboard Page",
+               type: :feature do
+
+  let!(:user) do
+    User.create(
+      name: "foosta",
+      email: "foosta@bubba.com",
+      password: "foosta",
+      password_confirmation: "foosta"
+    )
+  end
+
+  scenario "current user's dashboard should,
+    have a carousel with favorite recipes,
+    and buttons that add recipe or view all" do
+      visit dashboard_index_path
+      expect(page).to have_content(user.name)
+      expect(page).to have_content("Here are some things you can do.")
+      click_link "Add Your Recipe"
+      expect(page).to have_content("New Recipe")
+      visit dashboard_index_path
+      expect(page).to have_content("Here are some of your favorite recipes:")
+      click_link "View All Recipes"
+      expect(page).to have_content("Some recipes for you:")
+  end
+end
