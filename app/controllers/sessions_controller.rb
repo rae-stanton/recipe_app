6dbc6e639340aba8ea505fe9953b4cd15fcb2ec4
Rae Stanton <raestanton93@gmail.com>
2023-04-03 19:52:33 -0700

    Add login/logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,7 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:email])
+
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to dashboard_index_path, notice: "Logged in!"
@@ -14,7 +15,6 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    redirect_to root_path, notice: "Blah"
+    redirect_to root_path, notice: "Logged out"
   end
-
 end
